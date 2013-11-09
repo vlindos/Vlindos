202942c2b84efa2be7d23e64b2e7fd3ef82e4086
Vladimir Moushkov <vlindos@abv.bg>
2013-11-10 00:44:14 +0200

    work

---
+++ Vlindos.Common/CommadLine/ApplicationArgumentsGetter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace Vlindos.Common.CommadLine
+{
+    public interface IArgumentParsers
+    {
+        Dictionary<IApplicationArgument, List<string>> GetApplicationArguments();
+    }
+
+    public class ApplicationArgumentsGetter : IArgumentParsers
+    {
+        private readonly IApplicationArgument[] _applicationArguments;
+
+        public ApplicationArgumentsGetter(IApplicationArgument[] applicationArguments)
+        {
+            _applicationArguments = applicationArguments;
+        }
+
+        public Dictionary<IApplicationArgument, List<string>> GetApplicationArguments()
+        {
+            var dictionary = new Dictionary<IApplicationArgument, List<string>>();
+            var values = new List<string>();
+            var args = System.Environment.GetCommandLineArgs();
+
+            foreach (var applicationArgument in _applicationArguments)
+            {
+                for (var i = 0; i < args.Length; i++)
+                {
+                    var arg = args[i];
+                    if (arg.ToLowerInvariant() != applicationArgument.ShortCommand.ToLowerInvariant() &&
+                        arg.ToLowerInvariant() != applicationArgument.LongCommand.ToLowerInvariant()) continue;
+                    if (i + 1 >= args.Length) continue;
+                    i++;
+                    values.Add(args[i]);
+                }
+                dictionary.Add(applicationArgument, values);
+            }
+
+            return dictionary;
+        }
+    }
+}
---
+++ Vlindos.Common/CommadLine/IApplicationArgument.cs
@@ -0,0 +1,12 @@
+﻿namespace Vlindos.Common.CommadLine
+{
+    public interface IApplicationArgument
+    {
+        string Id { get; set; }
+        string ShortCommand { get; set; }
+        string LongCommand { get; set; }
+        bool ExpectsValue { get; set; }
+        string DefaultValue { get; set; }
+        string HelpMessage { get; set; }
+    }
+}
