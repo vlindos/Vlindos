0246b07d6a55d6c468149e5546f1bf3702d076fb
vladimirmoushkov <vladimir.moushkov@gmail.com>
2013-11-25 19:08:20 +0200

    work

---
+++ Database/Datatypes/IDatatype.cs
@@ -0,0 +1,11 @@
+﻿namespace Database.Datatypes
+{
+    public interface ILogicalOperation
+    {
+    }
+
+    public interface IDatatype<T> where T : struct
+    {
+        ILogicalOperation[] LogicalOperations { get; set; }
+    }
+}
---
+++ Database/IDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public interface IDatabase
+    {
+        string Namespace { get; set; }
+
+        IDatabaseHandle Open(string filepath);
+
+        IDatabaseHandle Handle { get; set; }
+    }
+
+    public interface IDatabaseHandle : IDisposable
+    {
+
+    }
+
+    public interface IOperation
+    {
+        IOperationType OperationType { get; set; }
+    }
+
+    public interface IOperationType
+    {
+    }
+
+    public interface IOperationResult
+    {
+    }
+}
